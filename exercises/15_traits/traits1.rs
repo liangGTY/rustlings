3d8cb0aa1f74ff6fbf607be9c23d6ae3c9ed54e4
liangtian06 <liangtian06@kuaishou.com>
2024-02-04 17:24:57 +0800

    generics & traits

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -7,14 +7,20 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
+    fn append_bar(self) -> Self {
+        let mut source = self.to_string();
+
+        source.push_str("Bar");
+
+        source
+    }
     // TODO: Implement `AppendBar` for type `String`.
+
 }
 
 fn main() {
