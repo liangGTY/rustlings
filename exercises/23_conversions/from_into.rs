89bfc547015f4a9300cdccb9ee647eab41d8bd37
liangtian06 <liangtian06@kuaishou.com>
2024-02-06 11:10:07 +0800

    clippy & conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+// use std::iter::TrustedRandomAccessNoCoerce;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,10 +42,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let vec: Vec<&str> = s.split(",").collect();
+
+        match vec[..] {
+            [x, y, ..] => {
+                if x.is_empty() {
+                    Person::default()
+                } else if let Ok(age) = y.parse() {
+                    Person {
+                        name: x.to_string(),
+                        age,
+                    }
+                } else {
+                    Person::default()
+                }
+            }
+            _ => {
+                Person::default()
+            }
+        }
     }
 }
 
@@ -59,6 +78,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_default() {
         // Test that the default person is 30 year old John
@@ -66,6 +86,7 @@ mod tests {
         assert_eq!(dp.name, "John");
         assert_eq!(dp.age, 30);
     }
+
     #[test]
     fn test_bad_convert() {
         // Test that John is returned when bad string is provided
@@ -73,6 +94,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 30);
     }
+
     #[test]
     fn test_good_convert() {
         // Test that "Mark,20" works
@@ -80,6 +102,7 @@ mod tests {
         assert_eq!(p.name, "Mark");
         assert_eq!(p.age, 20);
     }
+
     #[test]
     fn test_bad_age() {
         // Test that "Mark,twenty" will return the default person due to an
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,36 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let vec: Vec<&str> = s.split(",").collect();
+
+        match vec[..] {
+            [x] => {
+                if x.is_empty() {
+                    Err(ParsePersonError::Empty)
+                } else {
+                    Err(ParsePersonError::BadLen)
+                }
+            }
+            [x, y] => {
+                if x.is_empty() {
+                    return Err(ParsePersonError::NoName);
+                }
+                match y.parse() {
+                    Ok(age) => {
+                        Ok(Person {
+                            name: x.to_string(),
+                            age,
+                        })
+                    }
+                    Err(err) => {
+                        Err(ParsePersonError::ParseInt(err))
+                    }
+                }
+            }
+            _ => {
+                Err(ParsePersonError::BadLen)
+            }
+        }
     }
 }
 
@@ -68,6 +96,7 @@ mod tests {
     fn empty_input() {
         assert_eq!("".parse::<Person>(), Err(ParsePersonError::Empty));
     }
+
     #[test]
     fn good_input() {
         let p = "John,32".parse::<Person>();
@@ -76,6 +105,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 32);
     }
+
     #[test]
     fn missing_age() {
         assert!(matches!(
